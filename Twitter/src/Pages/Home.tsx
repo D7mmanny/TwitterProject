fe8a5e4028ed6e0db05ee0cc1a15547086449511
D7mmanny <d7mmanny@gmail.com>
2023-08-31 23:20:06 +0300

    trending

--- Twitter/src/Pages/Home.tsx
+++ Twitter/src/Pages/Home.tsx
@@ -143,6 +143,7 @@ function Home() {
 
             {/* The middle side */}
             <div className=" w-5/12 h-screen"></div>
+
             {/* Right side */}
             <div className=" w-4/12 h-screen border-solid border-x-4  border-b-slate-950 pl-5 ">
                 {/* search box */} 
@@ -155,7 +156,26 @@ function Home() {
                     </button>
                     <input type="text" className=" pl-4 outline-none w-2/3 bg-zinc-200" placeholder="Search Twitter" />
                 </div>
-                <div></div>
+                {/* trending */}
+                <div className="border-solid border-2 r w-5/6 h-4/5 mt-5 rounded-2xl  bg-zinc-200">
+                <div className="flex justify-between">
+                    <h1 className="text-xl px-5 pt-2"><b> Our accounts</b></h1>
+                    <svg 
+                    className="m-2 h-6 w-6 text-sky-500" 
+                    fill="none" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    stroke-width="2" 
+                    stroke="currentColor" 
+                    viewBox="0 0 24 24">
+                        <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
+                            </path>
+                            <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">
+                                </path>
+                                </svg>
+
+                </div>
+                </div>
             </div>
         </div>
         </div>
