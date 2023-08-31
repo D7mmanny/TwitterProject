27f9ff95c27d1c6378d668bc81510e82a9f0a221
D7mmanny <d7mmanny@gmail.com>
2023-08-31 22:33:40 +0300

    search-box

--- Twitter/src/Pages/Home.tsx
+++ Twitter/src/Pages/Home.tsx
@@ -144,7 +144,19 @@ function Home() {
             {/* The middle side */}
             <div className=" w-5/12 h-screen"></div>
             {/* Right side */}
-            <div className=" w-4/12 h-screen border-solid border-x-4 flex border-b-slate-950 "></div>
+            <div className=" w-4/12 h-screen border-solid border-x-4  border-b-slate-950 pl-5 ">
+                {/* search box */} 
+                <div className="border-solid border-2 r w-5/6 h-12 mt-5 rounded-full flex items-center bg-zinc-200">
+                    <button className="h-full pl-4 ">
+                        <svg className="h-5 w-5 fill-current" 
+                        viewBox="0 0 56.966 56.966">
+                            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
+                        </svg>
+                    </button>
+                    <input type="text" className=" pl-4 outline-none w-2/3 bg-zinc-200" placeholder="Search Twitter" />
+                </div>
+                <div></div>
+            </div>
         </div>
         </div>
     );
