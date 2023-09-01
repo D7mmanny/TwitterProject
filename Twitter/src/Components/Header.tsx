f3c27d67c45d7ed48cc5fad376491610b9a31230
shazmend <engshathaalfqih@gmail.com>
2023-09-01 23:17:28 +0300

    profile

--- Twitter/src/Components/Header.tsx
+++ Twitter/src/Components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     const name = localStorage.getItem("name");
   return (
     <div>
-      <div>
+      <div className="border">
         {/* Header photo */}
         <div className="bg-myBlue h-32 relative"></div>
         <img 
@@ -14,9 +14,16 @@ function Header() {
                 src={url} alt="" />
 
         {/* Info */}
-        <div className="mt-16 ml-9">
-            <div className="text-xl font-bold"> {name}</div>
-            <div><p className="text-sm">@{userName}</p></div>
+        <div className="mt-16 ml-9 flex flex-col gap-3">
+            <div>
+                <div className="text-xl font-bold"> {name}</div>
+                <div><p className="text-sm">@{userName}</p></div>
+            </div>
+            <div className="flex gap-2 text-sm pb-4">
+                <p><b>122</b> following </p>
+                <p><b>102</b> followers </p>
+            </div>
+            
         </div>
       </div>
     </div>
--- Twitter/src/Components/Profile.tsx
+++ Twitter/src/Components/Profile.tsx
@@ -4,43 +4,48 @@ import React from "react";
 import Header from "./Header";
 
 function Profile() {
-
-// const userName = localStorage.getItem("username");
-  const name = localStorage.getItem("name");
-
-
-
+const name = localStorage.getItem("name");
+const [active , setActive] = React.useState("posts")
 
   return (
     <div>
       {/* title */}
       <div className="flex items-center justify-between border-soled border-b-2 p-5  ">
         <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl">
-          {" "}
-          <b>{name}</b>
-        </h1>
-        <p className="text-xs">number of posts</p>
+          <h1 className="text-2xl">
+            {" "}
+            <b>{name}</b>
+          </h1>
+          <p className="text-xs">number of posts</p>
         </div>
         <a href="/Home">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 cursor-pointer"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-          />
-        </svg>
-        </a>  
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </a>
       </div>
+      <Header />
 
-      <Header/>
+      {/* Navigate */}
+      <div>
+        <ul className="flex justify-center gap-72 pt-4 font-semibold">
+          <a className=" cursor-pointer hover:text-sky-600" onClick={()=>{setActive("posts")}}><li>Posts</li></a>
+          <a className=" cursor-pointer hover:text-sky-600" onClick={()=>{setActive("likes")}}><li>Likes</li></a> 
+        </ul>
+      </div>
+      {/* {active == "posts" && } */}
+      {/* {active == "Likes" && } */}
     </div>
   );
 }
